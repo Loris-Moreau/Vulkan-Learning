c11d8eca8f146966d23ca64311f7c0d3f02051ad
Loris M. <80474693+Loris-Moreau@users.noreply.github.com>
2025-03-25 13:44:39 +0100

    [add] basic constraints
    03. p24

---
+++ Physics Intro/code/Constraint.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Math/Matrix.h"
+
+class Body;
+
+class Constraint
+{
+public:
+    virtual void PreSolve ( const float dt_sec) {}
+    virtual void Solve () {}
+    virtual void PostSolve () {}
+    
+protected:
+    MatMN GetInverseMassMatrix () const;
+    VecN GetVelocities () const;
+    void ApplyImpulses (const VecN & impulses);
+    
+public:
+    Body * m_bodyA;
+    Body * m_bodyB;
+    Vec3 m_anchorA;   // The anchor location in bodyA’s space
+    Vec3 m_axisA;    // The axis direction in bodyA’s space
+    Vec3 m_anchorB; // The anchor location in bodyB’s space
+    Vec3 m_axisB;  // The axis direction in bodyB’s space
+};
